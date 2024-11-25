0a5a46ebb6d90373333cbb29d42e5221a148fdf6
Daniela-2605 <daniela.gomez02@unicatolica.edu.co>
2024-11-25 16:14:31 -0500

    Hola1

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './Database/mongo.js'; // Ruta de tu archivo mongo.js
 import userRoutes from './routes/userRoutes.js'; // Ruta de las rutas para usuarios
+import cors from 'cors'; // Importar el módulo de CORS
 
 dotenv.config(); // Cargar variables de entorno
 
 const app = express();
+
+// Configuración de CORS
+app.use(cors({
+  origin: 'https://front-video-max.vercel.app', // Dominio del frontend en Vercel
+  credentials: true, // Habilitar credenciales si las necesitas
+}));
+
 app.use(express.json()); // Para procesar JSON
 
 connectDB(); // Conectar a MongoDB
